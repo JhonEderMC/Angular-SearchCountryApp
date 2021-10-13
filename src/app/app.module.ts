9ba250c29b3126a2401b23bcd77d61d921c429a3
PSOFKA0477\jhon mosquera <jhon.mosquera@sofka.com.co>
2021-10-12 21:51:51 -0500

    Created rotuing moduel and add routes to: capital,region, country/:id and generics **

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CountryModule } from './country/country.module';
+import { AppRoutingModule } from './app-routing.moudel';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     SharedModule,
-    CountryModule
+    CountryModule   
   ],
   providers: [],
   bootstrap: [AppComponent]
